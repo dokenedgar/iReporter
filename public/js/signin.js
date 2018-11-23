9d6d753a565813b410803f1559d633c4c7bb49d2
Doken Edgar <dokenedgarjr@gmail.com>
2018-11-23 15:06:36 +0100

    Create Admin Login Page

---
+++ public/js/signin.js
@@ -0,0 +1,9 @@
+//window.location.href = 'https://dokenedgar.herokuapp.com/signin'
+
+let signIn = () => {
+  window.location.href = 'profile.html'  
+}
+
+let signUp = () => {
+    window.location.href = 'profile.html'  
+  }
