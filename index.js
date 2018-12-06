61c4240f202bdba7669dd62ae1e5bc39bca78d6a
Doken Edgar <dokenedgarjr@gmail.com>
2018-12-06 15:59:04 +0100

    chore(package.json): add script
    add a script to run build after heroku has finished installing
    dependencies
    
    [Finishes: 162474940]

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 import express from 'express';
 import path from 'path';
-import User from './server/src/controllers/userController';
+import  User  from './server/src/controllers/userController';
 import RedFlag from './server/src/controllers/redflagController';
 import Intervention from './server/src/controllers/interventionController';
 import Admin from './server/src/controllers/adminController';
 
+// import  userroutes  from './server/src/routes/userRoutes';
 const app = express();
 app.use(express.json());
 
-//app.set('port', 3030);
+app.set('port', 3030);
+
+// app.use('/api/v1/auth', userroutes);
 app.set('port', process.env.PORT || 3030);
 
 // Sign-in & Sign-up
@@ -33,5 +36,6 @@ app.delete('/api/v1/interventions/:id', Intervention.deleteIntervention);
 
 app.patch('/api/v1/admin/:id', Admin.editStatus);
 //app.listen(3030, () => console.log('Listening on Port 3030...'));
+
 app.listen(app.get('port'));
 export default app;
---
+++ server/src/routes/userRoutes.js
@@ -0,0 +1,14 @@
+//{ }
+import express from 'express';
+//import { User } from './'
+
+const userroutes = express.Router();
+/*
+userroutes.get('/', (req, res) => {
+    res.json({ hi: 'we good'});
+ });
+ */
+
+//userroutes.post('signup', User)
+
+ module.exports = userroutes;
