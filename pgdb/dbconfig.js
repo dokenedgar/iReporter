4905d7c091b685d40a54a78852a27f56a14df97e
Doken Edgar <dokenedgarjr@gmail.com>
2018-12-12 23:36:54 +0100

    chore(dbconfig): add connection string
    - add a connection string property to the Pool, when creating pools
    The property value is obtained from the .env file
    
    [Finishes: #162597190]

--- pgdb/dbconfig.js
+++ pgdb/dbconfig.js
@@ -2,13 +2,13 @@ const Pool = require('pg').Pool;
 const dotenv = require('dotenv');
 
 dotenv.config();
-/*
+
 const pool = new Pool({
 	connectionString: process.env.DATABASE_URL
 	});
-	*/
+	
 
-const pool = new Pool();
+//const pool = new Pool();
 
 module.exports = {
   query : (text, params, callback) => {
